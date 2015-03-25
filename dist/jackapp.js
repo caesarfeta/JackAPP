b5f4388f60a9a3435317f4a17dc76eb13fe5743b
caesarfeta <adamtavares@gmail.com>
2015-03-25 14:21:28 -0400

    Added resize directive.

--- dist/jackapp.js
+++ dist/jackapp.js
@@ -281,3 +281,32 @@ function(){
 		templateUrl:  'ui/image-grid.ui.html'
    }
 });
+
+
+app.directive( 'resize', 
+function( $window ){
+return function( scope, element, attr ){
+	
+	var w = angular.element( $window );
+	
+	scope.$watch( function(){
+		return {
+		    'h': w.height()
+		};
+	}, 
+	
+	function (nv, ov) {
+		scope.windowHeight = nv.h;
+		scope.windowWidth = nv.w;
+		scope.resizeOffset = function ( height ) {
+		    return { 
+					'height': (nv.h - height) + 'px'
+		    };
+		};
+	}, true);
+ 
+	w.bind('resize', function () {
+	    scope.$apply();
+	});
+	
+}}); 
